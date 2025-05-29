cc511d322d34142114e1e5ef4c173f615f193c74
knmensahdarkwa7 <annonymoussilence7@gmail.com>
2025-05-29 21:53:40 +0000

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,47 +1,41 @@
 print("Welcome to my calculator")
 
-operator = input('Which operator (q to quit): ')
+# Function definitions
+def add():
+    num1 = int(input('Pls enter the first number: '))
+    num2 = int(input('Pls enter the second number: '))
+    return num1 + num2
 
-ops = ['+','-','/','x']
+def sub():
+    num1 = int(input('Pls enter the first number: '))
+    num2 = int(input('Pls enter the second number: '))
+    return num1 - num2
 
-while  not operator == "q" and operator == '+'or'-'or'/'or'x':
-    if operator == '+':
-        def add():
-            num1 = int(input('Pls enter the first number: '))
-            num2 = int(input('Pls enter the second number: '))
-            return num1 + num2
+def expo():
+    num1 = int(input('Pls enter the first number: '))
+    num2 = int(input('Pls enter the second number: '))
+    return num1 * num2
 
-        print(add())
-        operator = input('Which operator (q to quit): ')# Call the function using parentheses
+def div():
+    num1 = int(input('Pls enter the first number: '))
+    num2 = int(input('Pls enter the second number: '))
+    if num2 == 0:
+        return "Error: Division by zero is not allowed!"
+    return num1 / num2
 
-    elif operator == '-':
-        def sub():
-            num1 = int(input('Pls enter the first number: '))
-            num2 = int(input('Pls enter the second number: '))
-            return num1 - num2
+# Operator selection
+operator = input('Which operator (+, -, *, /): ')
 
-        print(sub())
-        operator = input('Which operator (q to quit): ')# Call the function using parentheses
+# Dictionary mapping operators to functions
+operations = {
+    '+': add,
+    '-': sub,
+    '*': expo,  
+    '/': div
+}
 
-    elif operator == 'x':
-        def expo():
-            num1 = int(input('Pls enter the first number: '))
-            num2 = int(input('Pls enter the second number: '))
-            return num1 * num2
-
-        print(expo())
-        operator = input('Which operator (q to quit): ')# Call the function using parentheses
-
-    elif operator == '/':
-        def div():
-            num1 = int(input('Pls enter the first number: '))
-            num2 = int(input('Pls enter the second number: '))
-            return num1 / num2
-
-        print(div())
-        operator = input('Which operator (q to quit): ')# Call the function using parentheses
-    else:
-        print('invalid input') 
-        break
-
-print('bye')
+# Execute the selected operation
+if operator in operations:
+    print(operations[operator]())  # Calls the function from the dictionary
+else:
+    print("Invalid operator! Please use +, -, *, or /.")
